7abed688d8677c0149345fa4d8c86e8f44b0f7dd
fugmando <joe@joeleedesign.com>
2012-10-26 18:41:33 -0400

    new stuff, no sweat

--- newfile.php
+++ newfile.php
@@ -3,7 +3,30 @@
 		<title>scrap</title>
 	</head>
 	<body>
-	learning how to use git
+	<?php  
+		$result = "u'28.7200000000', u'-88.3800000000', u'B23', u'2010-05-16 00:00', u'50', u'50', u'Brooks-McCall Cruise 02 MAY 15-17 2010', u'0.410000000000";
+		$myArray = explode(",", $result);
+		?>
+		<pre><?php print_r($myArray); ?></pre>
+		<?php foreach ($myArray as $value) {
+			
+		}?>
+	<p>
+First Name: <span id="fname"></span><br> 
+Last Name: <span id="lname"></span><br> 
+</p> 
+<script>
+var txt = '{"employees":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+
+var obj = eval ("(" + txt + ")");
+
+document.getElementById("fname").innerHTML=obj.employees[1].firstName 
+document.getElementById("lname").innerHTML=obj.employees[1].lastName 
+</script>
+	
 	<?php $array2 = array(6,"fox", "dog", array("x", "y", "z")); ?>
 	<?php echo $array2[3][1]; ?>
 	<?php $array2[2] = "cat"?>
@@ -17,6 +40,9 @@
 	output: Joe Lee
 	<?php $array3["firstname"] = "Larry"; ?>
 	<?php echo $array3["firstname"] . " " . $array3["lastname"]; ?>
+	
+	
+	
 	
 	<pre><?php print_r($array2); ?></pre>
 	
